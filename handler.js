78e6a29ad4ecd23da5b1230c389b9147ba1c131e
xerxesxe <jonas.hettwer@gmail.com>
2019-07-26 19:09:20 +0200

    added function

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@ const request = require('axios');
 const {extractListingsFromHTML} = require('./helpers');
 
 module.exports.linkedinscrapper = (event, context, callback) => {
-  request('https://www.thedonkeysanctuary.org.uk/vacancies')
+  request('https://www.zgs-consult.de/aktuelles/?tx_news_pi1%5BoverwriteDemand%5D%5Bcategories%5D=1&cHash=9529b5226579c5b717c0b795dc74af06')
     .then(({data}) => {
       const jobs = extractListingsFromHTML(data);
       callback(null, {jobs});
--- helpers.js
+++ helpers.js
@@ -3,18 +3,20 @@ const moment = require('moment');
 
 function extractListingsFromHTML (html) {
   const $ = cheerio.load(html);
-  const vacancyRows = $('.view-Vacancies tbody tr');
+  const vacancyRows = $('.col-md-12');
 
   const vacancies = [];
   vacancyRows.each((i, el) => {
 
     // Extract information from each row of the jobs table
-    let closing = $(el).children('.views-field-field-vacancy-deadline').first().text().trim();
-    let job = $(el).children('.views-field-title').first().text().trim();
-    let location = $(el).children('.views-field-name').text().trim();
-    closing = moment(closing.slice(0, closing.indexOf('-') - 1), 'DD/MM/YYYY').toISOString();
+    let date = $(el).children('.extra').children('time').first().text().trim();
+    let type = $(el).children('.extra').children('span').first().text().trim();
+    let title = $(el).children('h2').first().text().trim();
+    let description = $(el).children('div').children('p').first().text().trim();
+    let link = $(el).children('div').children('.internal-link').first().text().trim();
+    //closing = moment(closing.slice(0, closing.indexOf('-') - 1), 'DD/MM/YYYY').toISOString();
 
-    vacancies.push({closing, job, location});
+    vacancies.push({date, type, title, description, link});
   });
 
   return vacancies;
