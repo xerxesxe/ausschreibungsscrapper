411339ebe1a974eee1da6e01f8192da38c86ebe9
xerxesxe <jonas.hettwer@gmail.com>
2019-07-27 09:02:35 +0200

    handler edit

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 const request = require('axios');
 const {extractListingsFromHTML} = require('./helpers');
 
-module.exports.linkedinscrapper = (event, context, callback) => {
+module.exports.scrapper = (event, context, callback) => {
   request('https://www.zgs-consult.de/aktuelles/?tx_news_pi1%5BoverwriteDemand%5D%5Bcategories%5D=1&cHash=9529b5226579c5b717c0b795dc74af06')
     .then(({data}) => {
       const jobs = extractListingsFromHTML(data);
